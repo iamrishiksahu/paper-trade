8ef96c3d6af5cfdfbfc6da9504bd66ac06b5ba7c
Rishik Sahu <rishikkr@hotmail.com>
2024-03-16 16:44:17 +0530

    updated cors for deployed frontend 2

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,9 +2,9 @@ const whitelist = [
     'https://www.rishiksahu.in/',
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://paper-trade-backend.vercel.app/',
-    'https://paper-trader-backend.onrender.com/',
-    'https://papertrade.vercel.app/'
+    'https://papertrade.vercel.app/',
+    'https://paper-trade-eight.vercel.app/',
+    'https://papertrade.vercel.app',
 ]
 
 module.exports = whitelist
